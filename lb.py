c5ccc0e2dc9168c16efa6ba6e94d8837c414ba57
Mashfir Mohiuddin <mashfir@stripe.com>
2021-05-30 10:19:02 -0700

    take simple input

--- lb.py
+++ lb.py
@@ -1,5 +1,11 @@
+from html import HTML
+
 def main():
-    print("hello world")
+    h = HTML()
+    title = input("enter title of post: ")
+    h_title = h.h1(title)
+    print("title is: " + title)
+    print("Marked up title is: " + h_title)
 
 
 if __name__ == "__main__":
