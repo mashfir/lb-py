e5b80b618aabdbf995a54b23be68b0a549181fe1
Mashfir Mohiuddin <mashfir@stripe.com>
2021-06-06 15:51:45 -0700

    changed script to read write from db

--- lb.py
+++ lb.py
@@ -4,6 +4,8 @@ import re
 import subprocess
 import csv
 import sys
+import sqlite3
+from contextlib import closing
 
 
 def get_input():
@@ -23,25 +25,29 @@ def clean_filename(title: str):
     fname = re.sub(r"\s+", "-", fname) + ".html"
     return fname
 
+
 def check_file_exists(filename: str) -> bool:
-    with open("posts/postlist.tsv") as postlist:
-        post_reader = csv.reader(postlist, delimiter="\t", quotechar='"')
-        for row in post_reader:
-            if row[1] == filename:
-                postlist.close()
-                return True
-        postlist.close()
-        return False
+    with closing(sqlite3.connect("blog.db", isolation_level=None)) as connection:
+        with closing(connection.cursor()) as cursor:
+            matches = cursor.execute(
+                "SELECT path FROM posts WHERE path = ?",
+                (filename,),
+            ).fetchall()
+            return len(matches) > 0
+
 
 def add_new_post(title: str, filename: str) -> None:
     if check_file_exists(filename):
         sys.exit("Filename already exists. Please delete entries and try again.")
     else:
-        with open("posts/postlist.tsv", "a") as postlist:
-            post_writer = csv.writer(postlist, delimiter="\t", quotechar='"', quoting=csv.QUOTE_NONNUMERIC)
-            post_time = datetime.datetime.now().strftime("%a, %d %b %Y %H:%M:%S %Z")
-            post_writer.writerow([title, filename, post_time])
-            postlist.close()
+        with closing(sqlite3.connect("blog.db", isolation_level=None)) as connection:
+            with closing(connection.cursor()) as cursor:
+                post_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S %Z")
+                matches = cursor.execute(
+                    "INSERT INTO posts VALUES(?, ?, ?, NULL)",
+                    (title, filename, post_time),
+                )
+
 
 def generate_index() -> None:
     publish_date = datetime.datetime.now().strftime("%b. %d, %Y")
@@ -49,15 +55,20 @@ def generate_index() -> None:
     with open("index.html", "w") as idx:
         idx.write(header + publish_date)
         idx.write("<ul>\n")
-        with open("posts/postlist.tsv") as postlist:
-            post_reader = csv.DictReader(postlist, delimiter="\t", quotechar='"')
-            for row in post_reader:
-                entry = "<li>{}: <a href='posts/{}' target='_blank'>{}</a></li>\n".format(row['published'], row['filename'], row['title'])
-                idx.write(entry)
-            postlist.close()
+        with closing(sqlite3.connect("blog.db", isolation_level=None)) as connection:
+            with closing(connection.cursor()) as cursor:
+                posts = cursor.execute(
+                    "SELECT * FROM posts ORDER BY published DESC"
+                ).fetchall()
+        for row in posts:
+            entry = "<li>{}: <a href='posts/{}' target='_blank'>{}</a></li>\n".format(
+                row[2], row[1], row[0]
+            )
+            idx.write(entry)
         idx.write("</ul>\n")
         idx.write(footer)
         idx.close()
+
 
 def read_boilerplate() -> (str, str):
     header = open(os.path.join("templates/", "header.html")).read()
@@ -73,9 +84,11 @@ def create_post():
         f.write(header + title + date + post + footer)
         f.close()
 
+
 def main():
     # parse out arguments based on user inputs to perform specific actions
     pass
+
 
 if __name__ == "__main__":
     create_post()
